91140a5dbf0f61051dc4d7f5ebecfb6ada887932
isaiah <isaiahballah@gmail.com>
2018-12-31 03:38:58 -0500

    fixed merge conflicts...

--- lib/assignmentList.dart
+++ lib/assignmentList.dart
@@ -97,7 +97,6 @@ class _AssignmentWidgetState extends State<AssignmentWidget> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return ExpansionTile(
       title: Text("Assignment", textScaleFactor: 1.5,),
       leading: Text("weight " + (widget.gradable.weight * 100).truncateToDouble().toString() + "%"),
@@ -128,58 +127,6 @@ class _AssignmentWidgetState extends State<AssignmentWidget> {
       ],
     //      leading: RaisedButton(child:Text("Edit"), onPressed: _onEditPressed),
     //      trailing: RaisedButton(child:Text("Delete"), onPressed: _onDeletePressed)
-=======
-    return Dismissible(
-
-        key: Key(widget.gradable.name + "#" +
-            widget.gradable.grade.truncate().toString()),
-
-        background: Container(color: Colors.red),
-
-        onDismissed: (direction) {
-          widget.remove(widget.gradable);
-        },
-
-        child: ExpansionTile(
-          title: Text("Assignment", textScaleFactor: 1.5,),
-          leading: Text("weight " +
-              (widget.gradable.weight * 100).truncateToDouble().toString() +
-              "%"),
-          trailing: Text(
-              widget.gradable.grade.truncateToDouble().toString() + " % ",
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .display1),
-
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Expanded(flex: 4, child: RaisedButton(
-                      child: Text(
-                          "Rename", style: TextStyle(color: Colors.white)),
-                      color: Colors.green,
-                      onPressed: () => _showRenameDialog(context)
-                  )),
-
-                  Spacer(),
-                  Expanded(flex: 4, child: RaisedButton(
-                      child: Text(
-                          "Delete", style: TextStyle(color: Colors.white)),
-                      color: Colors.red,
-                      onPressed: _onDeletePressed)
-                  ),
-
-                ],
-              ),
-
-            )
-          ],
-        )
->>>>>>> f945f95a16ac065a50d9ac5f0597dcb3363d4a5b
     );
   }
 }
